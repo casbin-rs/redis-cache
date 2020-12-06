0900d02156c84eb420a5c56398b3f692154143dc
Cheng JIANG <alex_cj96@foxmail.com>
2020-12-06 15:07:20 +0100

    upgrade redis & use hset

--- src/lib.rs
+++ src/lib.rs
@@ -5,28 +5,22 @@ use serde::{de::DeserializeOwned, Serialize};
 
 use std::{borrow::Cow, hash::Hash, marker::PhantomData};
 
-const CACHE_PREFIX: &str = "casbin";
+const CACHE_HKEY: &str = "casbin_cache";
 
-pub struct RedisCache<K, V>
-where
-    K: Eq + Hash + Send + Sync + Serialize + 'static,
-    V: Send + Sync + Clone + Serialize + DeserializeOwned + 'static,
-{
+pub struct RedisCache<K, V> {
     conn: Connection,
+    cap: usize,
     _marker: PhantomData<(K, V)>,
 }
 
-impl<K, V> RedisCache<K, V>
-where
-    K: Eq + Hash + Send + Sync + Serialize + 'static,
-    V: Send + Sync + Clone + Serialize + DeserializeOwned + 'static,
-{
+impl<K, V> RedisCache<K, V> {
     pub fn new<U: IntoConnectionInfo>(url: U) -> RedisResult<RedisCache<K, V>> {
         let client = Client::open(url)?;
         let conn = client.get_connection()?;
 
         Ok(RedisCache {
             conn,
+            cap: 1000,
             _marker: PhantomData,
         })
     }
@@ -37,18 +31,18 @@ where
     K: Eq + Hash + Send + Sync + Serialize + 'static,
     V: Send + Sync + Clone + Serialize + DeserializeOwned + 'static,
 {
-    // This function shouldn't be implemented, users can configure redis maxmium memory & lru
-    // elimination strategy to achive lru-cache similar effect
-    fn set_capacity(&mut self, _c: usize) {}
+    fn set_capacity(&mut self, cap: usize) {
+        self.cap = cap;
+    }
 
     fn get(&mut self, k: &K) -> Option<Cow<'_, V>> {
-        if let Ok(ser_key) = serde_json::to_string(&k) {
-            let cache_key = format!("{}::{}", CACHE_PREFIX, ser_key);
-            if let Ok(res) = redis::cmd("GET")
-                .arg(cache_key)
+        if let Ok(field) = serde_json::to_string(&k) {
+            if let Ok(res) = redis::cmd("HGET")
+                .arg(CACHE_HKEY)
+                .arg(field)
                 .query::<String>(&mut self.conn)
             {
-                return serde_json::from_str(&res).ok().map(|x| Cow::Owned(x));
+                return serde_json::from_str(&res).map(|x| Cow::Owned(x)).ok();
             }
         }
 
@@ -56,34 +50,42 @@ where
     }
 
     fn has(&mut self, k: &K) -> bool {
-        self.get(k).is_some()
+        if let Ok(field) = serde_json::to_string(&k) {
+            if let Ok(res) = redis::cmd("HEXISTS")
+                .arg(CACHE_HKEY)
+                .arg(field)
+                .query::<bool>(&mut self.conn)
+            {
+                return res;
+            }
+        }
+
+        false
     }
 
     fn set(&mut self, k: K, v: V) {
-        if let (Ok(ser_key), Ok(ser_val)) = (serde_json::to_string(&k), serde_json::to_string(&v)) {
-            let cache_key = format!("{}::{}", CACHE_PREFIX, ser_key);
-            let _ = redis::cmd("SET")
-                .arg(cache_key)
-                .arg(ser_val)
-                .query::<String>(&mut self.conn);
+        if let Ok(keys) = redis::cmd("HKEYS")
+            .arg(CACHE_HKEY)
+            .query::<Vec<String>>(&mut self.conn)
+        {
+            if keys.len() < self.cap {
+                if let (Ok(field), Ok(value)) =
+                    (serde_json::to_string(&k), serde_json::to_string(&v))
+                {
+                    let _ = redis::cmd("HSET")
+                        .arg(CACHE_HKEY)
+                        .arg(field)
+                        .arg(value)
+                        .query::<bool>(&mut self.conn);
+                }
+            }
         }
     }
 
     fn clear(&mut self) {
-        let cache_key = format!("{}::*", CACHE_PREFIX);
-
-        let script = format!(
-            r#"
-            EVAL "for i, name in ipairs(redis.call('KEYS', '{}')) do redis.call('del', name, 0); end" 0
-        "#,
-            cache_key
-        );
-
-        let script = redis::Script::new(&script);
-        let _ = script
-            .arg(1)
-            .arg(2)
-            .invoke::<Option<String>>(&mut self.conn);
+        let _ = redis::cmd("DEL")
+            .arg(CACHE_HKEY)
+            .query::<String>(&mut self.conn);
     }
 }
 
@@ -92,20 +94,14 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_set_and_get() {
+    fn test_set_has_get_clear() {
         let mut cache = RedisCache::new("redis://localhost:6379").unwrap();
 
         cache.set(vec!["alice", "/data1", "read"], false);
         assert!(cache.has(&vec!["alice", "/data1", "read"]));
         assert!(cache.get(&vec!["alice", "/data1", "read"]) == Some(Cow::Borrowed(&false)));
-    }
-
-    #[test]
-    fn test_clear() {
-        let mut cache = RedisCache::new("redis://localhost:6379").unwrap();
-
-        cache.set(vec!["alice", "/data1", "read"], false);
         cache.clear();
+
         assert!(cache.get(&vec!["alice", "/data1", "read"]) == None);
     }
 }
